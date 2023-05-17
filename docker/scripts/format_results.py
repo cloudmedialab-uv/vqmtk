e71429edef5b60d3b5c93352ed63ec04dbc812ee
Juan Gutiérrez-Aguado <juan.gutierrez@uv.es>
2023-05-17 10:53:23 +0200

    Version with changes sugested by reviewers

--- docker/scripts/format_results.py
+++ docker/scripts/format_results.py
@@ -6,7 +6,6 @@ import json
 import numpy as np
 from glob import glob
 import pandas as pd
-import json
 import xml
 
 
@@ -26,23 +25,6 @@ def ls(expr='*.*'):
     return glob(expr)
 
 
-def get_itu(path_chunks):
-
-    chunk_files = ls(path_chunks+"/*")
-    mean_chunk = []
-
-    for chunk in chunk_files:
-
-        with open(chunk) as f:
-            data = json.load(f)
-        mos = list(list(data.items())[0][1]['video'].items())[0][1]
-
-        mean_chunk.append(np.mean(mos))
-
-    df = pd.DataFrame(mean_chunk, columns=['itu'])
-    return df
-
-
 def get_quality_metric(path_metric, ext):
 
     metric_files = ls(path_metric+"/*")
@@ -50,54 +32,34 @@ def get_quality_metric(path_metric, ext):
     df = pd.DataFrame()
     df_chunk = pd.DataFrame()
 
-    if len(metric_files) == 1 and 'itu' in metric_files[0]:
-       
-        df_itu = get_itu(metric_files[0])
-        index = list(range(1, len(df_itu)+1, 1))
-        df_itu.insert(loc=0, column='chunk', value=index)
-        df_itu.set_index('chunk', inplace=True)
-        printFile(df_itu, ext, "itu")
+    for metric in metric_files:
+        name_metric = os.path.basename(metric)
 
-    else:
-        for metric in metric_files:
-            name_metric = os.path.basename(metric)
-
-            if name_metric == "itu":
-                df_itu = get_itu(metric)
-                index = list(range(1, len(df_itu)+1, 1))
-                df_itu.insert(loc=0, column='chunk', value=index)
-                df_itu.set_index('chunk', inplace=True)
-                printFile(df_itu, ext, "itu")
-
-            else:
-                df_chunk = pd.read_csv(metric+"/chunks.csv")
-                df = pd.concat([df, df_chunk[name_metric]], axis=1)
-
-        index = list(range(1, len(df)+1, 1))
-        df.insert(loc=0, column='chunk', value=index)
-        df.set_index('chunk', inplace=True)
-
-        printFile(df, ext, "vqm")
-
-
-def printFile(df, ext, name):
-
-    if name == "vqm":
-        if ext == "json":
-            df.to_json(path_metric+"/vqmcli_results.json", index=True)
-        elif ext == "csv":
-            df.to_csv(path_metric+"/vqmcli_results.csv",
-                      encoding='utf-8', index=True)
+   
+        df_chunk = pd.read_csv(metric+"/chunks.csv")
+        if "siti" in name_metric:
+            df_chunk = df_chunk[['si', 'ti']].round(5) 
+            df = pd.concat([df, df_chunk['si']], axis=1)
+            df = pd.concat([df, df_chunk['ti']], axis=1)
         else:
-            df.to_xml(path_metric+"/vqmcli_results.xml")
-    else:
-        if ext == "json":
-            df.to_json(path_metric+"/vqmcli_itu.json", index=True)
-        elif ext == "csv":
-            df.to_csv(path_metric+"/vqmcli_itu.csv",
-                      encoding='utf-8', index=True)
-        else:
-            df.to_xml(path_metric+"/vqmcli_itu.xml")
+            df = pd.concat([df, df_chunk[name_metric]], axis=1)
+
+    index = list(range(1, len(df)+1, 1))
+    df.insert(loc=0, column='chunk', value=index)
+    df.set_index('chunk', inplace=True)
+
+    printFile(df, ext)
+
+
+def printFile(df, ext):
+
+   if ext == "json":
+       df.to_json(path_metric+"/vqmcli_results.json", index=True)
+   elif ext == "csv":
+       df.to_csv(path_metric+"/vqmcli_results.csv",
+               encoding='utf-8', index=True)
+   else:
+       df.to_xml(path_metric+"/vqmcli_results.xml")
 
 
 if __name__ == "__main__":
